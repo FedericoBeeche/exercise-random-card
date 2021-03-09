4060c0ab8ae100a85cca9309df63e5ccd1370b95
FedericoBeeche <fbeeche@gmail.com>
2021-03-09 02:45:08 +0000

    actualizacion del proyecto

---
+++ app.js
@@ -0,0 +1,30 @@
+let tipoCarta = ["♦", "♥", "♠", "♣"];
+let numeroCarta = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+// generador de numero
+function genNum() {
+    var numeroCartaIndex = Math.floor(Math.random() * (numeroCarta.length));
+    document.getElementById('numeroCarta').innerHTML = numeroCarta[numeroCartaIndex];
+}
+// generador de tipo de carta
+function genCardType() {
+    var tipoCartaIndex = Math.floor(Math.random() * (tipoCarta.length));
+    document.getElementById('parteArribaCarta').innerHTML = tipoCarta[tipoCartaIndex];
+    document.getElementById('parteAbajoCarta').innerHTML = tipoCarta[tipoCartaIndex];
+    if (tipoCartaIndex == 0 || tipoCartaIndex == 1) {
+        document.querySelector('.card').classList.add("red");
+    } else {
+        document.querySelector('.card').classList.add("black");
+    }
+}
+//intervalo para generar una nueva carta
+setInterval(randomCard, 10000)
+//boton que genera carta nueva
+var boton = document.getElementById('botoncito');
+boton.addEventListener('click', randomCard);
+
+function randomCard() {
+    genNum();
+    genCardType();
+}
+//esto genera la carta cuando carga la pagina
+randomCard();
